866afd26544f10953f45cd74abba7b35278dba52
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-10 21:18:50 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const makeChange = (c) => {
   // your name here
+	let pennyValue = 1 ;
+	let nickelValue = 5 ;
+	let dimeValue = 10 ;
+	let quarterValue = 25 ;
+
+	let pennies = 0 ;
+	let nickel = 0 ;
+	let dime = 0 ;
+	let quater = 0 ;
+	quater = Math.floor(c / quarterValue) ;
+	c %= quarterValue;
+	
+	dime = Math.floor(c / dimeValue) ;
+	c %= dimeValue;
+
+	nickel = Math.floor(c / nickelValue) ;
+	c %= nickelValue ;
+
+	pennies = c ;
+
+	let result = {
+		q :  quater,
+		
+		d :  dime,
+		
+        n :  nickel,
+		
+		p :  pennies
+	}
+	return result ;
 };
 
 // Do not the change the code below
